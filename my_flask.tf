95f6d4d106cf6fdf926ba94eeec2a2ec5755c488
Lior Ben Ami <liorbenami1@gmail.com>
2020-12-15 21:39:02 +0200

    add gitignore and try to ssh to ec2

--- my_flask.tf
+++ my_flask.tf
@@ -1,10 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
 provider "aws" {
-  region = "eu-west-2"
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "my-flask-app" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "my-flask-app"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      private_key = file("D:/devops/my_key.pem")
+      host = "${element(aws_instance.my-flask-app.*.public_ip, 0)}"
+    }
+    inline = [
+      "sudo amazon-linux-extras install ansible2 -y",
+      "sudo apt install git -y",
+      "git clone https://github.com/devops-school/ansible-hello-world-role /tmp/ans_ws",
+      "ansible-playbook /tmp/ans_ws/site.yaml"
+    ]
+  }
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.my-flask-app.id
+}
+
+
+/*
+provider "aws" {
+  region = "eu-west-1"
   shared_credentials_file = "~/.aws/credentials"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c09927662c939f41"
+  ami           = "ami-00ddb0e5626798373"
   instance_type = "t2.micro"
   
   tags = {
@@ -20,4 +65,4 @@ resource "aws_instance" "web" {
     ]
   }
 }
-
+*/
