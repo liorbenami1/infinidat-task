2417d57d4e7420c941bf4db0d27ea7090fced8b3
vagrant <vagrant@vagrant.vm>
2020-12-16 16:49:29 +0000

    Add home.html

--- flask-application/hello.py
+++ flask-application/hello.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 @app.route('/hi')
 def hello_world():
-    return 'Hello, \n my name is: Lior Ben Ami'
+#  return 'Hello, \n my name is: Lior Ben Ami'
+    return render_template("home.html")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
