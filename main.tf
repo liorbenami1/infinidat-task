863ec400762aa27af23ad52c3298d245b2fdf625
vagrant <vagrant@vagrant.vm>
2020-12-16 16:07:34 +0000

    add security group and enable ssh & http & https

--- main.tf
+++ main.tf
@@ -2,24 +2,70 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-#resource "aws_key_pair" "terraform-demo" {
-#  key_name   = "terraform-demo"
-#  public_key = file("terraform-demo.pub")
-#}
+resource "aws_security_group" "ubuntu" {
+  name        = "ubuntu-security-group"
+  description = "Allow HTTP, HTTPS and SSH traffic"
 
-resource "aws_instance" "my-instance" {
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
+resource "aws_instance" "ubuntu" {
 	ami = "ami-04169656fea786776"
-	instance_type = "t2.nano"
-	#key_name = "${aws_key_pair.terraform-demo.key_name}"
-	key_name = "my-key-pair.pem"
+	instance_type = "t2.micro"
+	key_name = "tmp-key-pair"
 	user_data = file("user_data.sh")
 	tags = {
-		Name = "Terraform"	
-		Batch = "5AM"
+		Name = "ubuntu"	
 	}
+
+        vpc_security_group_ids = [
+          	aws_security_group.ubuntu.id
+        ]
+
+  	connection {
+    		type        = "ssh"
+    		user        = "ubuntu"
+    		private_key = file("tmp-key-pair")
+  		#  private_key = file("key")
+    		host        = self.public_ip
+  	}
 }
+
 
 resource "aws_eip" "ip" {
   vpc      = true
-  instance = aws_instance.my-instance.id
+  instance = aws_instance.ubuntu.id
 }
